9aabd9f02401a57d5bb988cecb5859b09ec2f094
seungho <87319059+sts06017@users.noreply.github.com>
2024-06-19 17:39:18 +0900

    [level 0] Title: 모스부호 (1), Time: 0.09 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ letter:String) -> String {
+    let morse: [String: Character] = [
+    ".-": "a", "-...": "b", "-.-.": "c", "-..": "d", ".": "e", "..-.": "f",
+    "--.": "g", "....": "h", "..": "i", ".---": "j", "-.-": "k", ".-..": "l",
+    "--": "m", "-.": "n", "---": "o", ".--.": "p", "--.-": "q", ".-.": "r",
+    "...": "s", "-": "t", "..-": "u", "...-": "v", ".--": "w", "-..-": "x",
+    "-.--": "y", "--..": "z"
+    ]
+    var result = ""
+    result += letter.split(separator: " ").map{ morse[String($0)]! }
+    return result
+}

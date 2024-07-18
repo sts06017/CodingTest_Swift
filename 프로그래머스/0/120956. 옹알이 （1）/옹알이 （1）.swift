8c0d8a7ffda0c37fbea389c233d9e95603de608c
seungho <87319059+sts06017@users.noreply.github.com>
2024-07-18 17:45:43 +0900

    [level 0] Title: 옹알이 (1), Time: 2.18 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.swift
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.swift
@@ -2,12 +2,10 @@ import Foundation
 
 func solution(_ babbling:[String]) -> Int {
     let dic = ["aya", "ye", "woo", "ma"]
-    var result0 = babbling.map{ $0.replacingOccurrences(of: dic[0], with: "0") }
-    var result1 = result0.map{ $0.replacingOccurrences(of: dic[1], with: "1") }
-    var result2 = result1.map{ $0.replacingOccurrences(of: dic[2], with: "2") }
-    var result3 = result2.map{ $0.replacingOccurrences(of: dic[3], with: "3")}
-    let result = result3.filter{ Int($0) != nil }
-    
-    return result.count
+    var babbling = babbling
+    for i in 0..<4 {
+        babbling = babbling.map{ $0.replacingOccurrences(of: dic[i], with: "0")}
+    }
+    return babbling.filter{Int($0) != nil}.count
 
 }

ca1ab87293788f5abb3a7f31f54c4a14b45d4491
seungho <87319059+sts06017@users.noreply.github.com>
2025-02-07 22:17:39 +0900

    [level 2] Title: 타겟 넘버, Time: 71.83 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
@@ -3,19 +3,20 @@ import Foundation
 func solution(_ numbers:[Int], _ target:Int) -> Int {
     var count = 0
     
-    func dfs(index: Int, sum: Int) -> Int {
-        var index = index+1
+    func dfs(index: Int, sum: Int) {
+        var index = index + 1
         
         if index == numbers.count {
             if sum == target {
                 count += 1
             }
-            return 0
-        } else {
-            dfs(index: index, sum: sum + numbers[index])
-            dfs(index: index, sum: sum - numbers[index])
-            return 0
+            return
         }
+        
+        dfs(index: index, sum: sum + numbers[index])
+        dfs(index: index, sum: sum - numbers[index])
+        
+        return
     }
     
     dfs(index: -1, sum: 0)

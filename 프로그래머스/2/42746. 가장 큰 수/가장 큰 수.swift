6a73b6bd7382835a1313330e78ef88f47ec62585
seungho <87319059+sts06017@users.noreply.github.com>
2025-03-20 16:09:02 +0900

    [level 2] Title: 가장 큰 수, Time: 2355.54 ms, Memory: 22.4 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.swift
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.swift
@@ -1,5 +1,31 @@
 import Foundation
 
+struct nums {
+    var firstNum: Int
+    var fullNum: Int
+}
 func solution(_ numbers:[Int]) -> String {
-    return ""
+    var numsArray: [nums] = []
+    
+    for number in numbers {
+        let first = Int(String(String(number).first!))!
+        var num = nums(firstNum: first, fullNum: number)
+        numsArray.append(num)
+    }
+    
+    numsArray.sort { a, b in
+        if a.firstNum == b.firstNum {
+            let ab = "\(a.fullNum)\(b.fullNum)"
+            let ba = "\(b.fullNum)\(a.fullNum)"
+            return Int(ab)! > Int(ba)!
+        } else {
+            return a.firstNum > b.firstNum
+        }
+    }
+    
+    if numsArray.first!.fullNum == 0 {
+        return "0"
+    } else {
+        return numsArray.map { String($0.fullNum) }.joined()   
+    }
 }

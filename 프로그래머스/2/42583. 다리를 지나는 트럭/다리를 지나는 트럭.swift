0b09e26fdbde4a41c776aca46ac70689a7fc90d0
seungho <87319059+sts06017@users.noreply.github.com>
2025-03-21 09:50:04 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 452.47 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.swift
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.swift
@@ -1,5 +1,26 @@
 import Foundation
 
-func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
-    return 0
+func solution(_ bridge_length: Int, _ weight: Int, _ truck_weights: [Int]) -> Int {
+    var time = 0
+    var queue = Array(repeating: 0, count: bridge_length)
+    var totalWeight = 0
+    var truckIndex = 0
+    
+    while truckIndex < truck_weights.count || totalWeight > 0 {
+        time += 1
+        totalWeight -= queue.removeFirst()
+        
+        if truckIndex < truck_weights.count { // 다음 트럭이 있을때
+            let nextTruck = truck_weights[truckIndex] // 다음 트럭의 무게
+            if totalWeight + nextTruck <= weight {  // 다음 트럭이 올라와도 무게가 넘지 안을때
+                queue.append(nextTruck) // 다음트럭이 올라감
+                totalWeight += nextTruck
+                truckIndex += 1
+            } else {    // 무게가 넘는다면
+                queue.append(0)
+            }
+        }
+    }
+    
+    return time
 }
